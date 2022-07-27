667fed47e8a7603b49fda0fb15dd5c85dcf841d9
Yaroslav Kuzyk <y.kuzyk@wpt.dev>
2022-07-27 20:00:40 +0300

    base vue app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  v-app
+    v-main
+      v-container
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+  }),
+};
+</script>
+<style lang="scss">
+</style>
